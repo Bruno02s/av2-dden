93ea8c7541874b65abdb6a270eb9916e560e04ae
Bruno02s <brunolourenco02s@gmail.com>
2025-04-30 20:07:58 -0300

    fix: ajustando numero da sorte e foramtacao das datas

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ app.get("/home/", function(req, res) {
 app.get("/data_servidor/", function(req, res) {
   const data = new Date();
 
-  res.send(`a data que esta no servidor eh: ${data}`)
+  const formatado = new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  }).format(data);
+
+  res.send(`a data que esta no servidor eh: ${formatado}`)
 })
 
 app.get("/numero_sorte/", function(req, res) {
-  const number = Math.random();
+  const number = Math.random() * (10 - 5) + 5;
 
   res.send(`seu numero da sorte eh: ${number}`)
 })
